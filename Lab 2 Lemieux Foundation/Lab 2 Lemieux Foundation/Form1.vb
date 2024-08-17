70661e0a4f63fcce6df8834400ea54c881614211
UniC <153264405+UniCres@users.noreply.github.com>
2024-08-16 21:57:47 -0700

    Delete Lab 2 Lemieux Foundation directory

--- Lab 2 Lemieux Foundation/Lab 2 Lemieux Foundation/Form1.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form1
-    'Shiyu Chen
-    '1/31/2023
-    '3rd Block
-    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-        Me.Close()
-    End Sub
-End Class
