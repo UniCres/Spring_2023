a84d948c63c1403b3150787b69a6288ceed34404
UniC <153264405+UniCres@users.noreply.github.com>
2024-08-16 22:43:54 -0700

    Add files via upload

---
+++ Apr.2023/Sub Function Example/Sub Function Example/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblArea = New System.Windows.Forms.Label()
+        Me.lblBase = New System.Windows.Forms.Label()
+        Me.txtBase = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Tan
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI Semibold", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnCalculate.Location = New System.Drawing.Point(137, 306)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(159, 54)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'lblArea
+        '
+        Me.lblArea.AutoSize = True
+        Me.lblArea.Font = New System.Drawing.Font("Segoe UI", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblArea.Location = New System.Drawing.Point(50, 28)
+        Me.lblArea.Name = "lblArea"
+        Me.lblArea.Size = New System.Drawing.Size(342, 40)
+        Me.lblArea.TabIndex = 1
+        Me.lblArea.Text = "Area of a Right Triangle"
+        '
+        'lblBase
+        '
+        Me.lblBase.AutoSize = True
+        Me.lblBase.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblBase.Location = New System.Drawing.Point(160, 95)
+        Me.lblBase.Name = "lblBase"
+        Me.lblBase.Size = New System.Drawing.Size(117, 25)
+        Me.lblBase.TabIndex = 2
+        Me.lblBase.Text = "Side 1 (Base)"
+        '
+        'txtBase
+        '
+        Me.txtBase.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtBase.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtBase.Location = New System.Drawing.Point(147, 135)
+        Me.txtBase.Name = "txtBase"
+        Me.txtBase.ReadOnly = True
+        Me.txtBase.Size = New System.Drawing.Size(140, 33)
+        Me.txtBase.TabIndex = 3
+        '
+        'txtHeight
+        '
+        Me.txtHeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtHeight.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtHeight.Location = New System.Drawing.Point(147, 232)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.ReadOnly = True
+        Me.txtHeight.Size = New System.Drawing.Size(140, 33)
+        Me.txtHeight.TabIndex = 5
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblHeight.Location = New System.Drawing.Point(148, 192)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(139, 25)
+        Me.lblHeight.TabIndex = 4
+        Me.lblHeight.Text = "Side 2 (Height)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.ClientSize = New System.Drawing.Size(441, 393)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.txtBase)
+        Me.Controls.Add(Me.lblBase)
+        Me.Controls.Add(Me.lblArea)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sub/Function Example"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblArea As Label
+    Friend WithEvents lblBase As Label
+    Friend WithEvents txtBase As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents lblHeight As Label
+End Class
---
+++ Apr.2023/Sub Function Example/Sub Function Example/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    'Shiyu Chen
+    '4/24/2023
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Call sub to prompt user for sides
+        prompt_user()
+
+        'Call function to calculate the area of the triangle
+        Dim decArea As Decimal
+        decArea = return_area()
+        'Output the area in msgbox
+        MsgBox("The area of your right triangle is: " & decArea, MsgBoxStyle.Information, "Area of Triangle")
+
+        'Call sub to clear out the form
+        clear_form()
+    End Sub
+
+    'Prompt user to enter values for sides (Base & Height)
+    Private Sub prompt_user()
+        'Add values to textboxes
+        txtBase.Text = InputBox("Please enter the base of a triangle", "Enter the Base")
+        txtHeight.Text = InputBox("Please enter the height of a triangle", "Enter the Height")
+    End Sub
+
+    'Calculate the area of the triangle
+    Private Function return_area() As Decimal
+        Dim decArea As Decimal
+        decArea = txtBase.Text * txtHeight.Text / 2
+        Return decArea
+    End Function
+
+    'Clear out the form
+    Private Sub clear_form()
+        txtBase.Clear()
+        txtHeight.Clear()
+    End Sub
+End Class
